dd9b0c0985fbc7bd8ab5984777293763bef87771
ZJTurner117 <153025651+ZJTurner117@users.noreply.github.com>
2023-12-12 15:27:07 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var square = document.getElementById("square")
+    square.addEventListener("dblclick", function(){
+        console.log(`square clicked`)
+    square.style.backgroundColor = "green"
+    })
+var square = document.getElementById("square")
+    square.addEventListener("mouseover", function(){
+    square.style.backgroundColor = "blue"
+    })    
+    var square = document.getElementById("square")
+    square.addEventListener("mousedown", function(){
+    square.style.backgroundColor = "green"
+    })
+var square = document.getElementById("square")
+    square.addEventListener("mouseup", function(){
+    square.style.backgroundColor = "yellow"
+    })
+var square = document.getElementById("square")
+    square.addEventListener("mousewheel", function(){
+    square.style.backgroundColor = "orange"
+    })
+    var square = document.getElementById("square")
+    square.addEventListener("keypress", function(){
+    square.inputMode("textInput")
+    })
+    
